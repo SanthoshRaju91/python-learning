bc298e0a37326550d4ffc38e2df3cacd9bbb71b6
Nagaraj Santhosh <santhosh.nagaraj@rakuten.com>
2020-01-26 21:09:22 +0530

    ex10: escape sequences

---
+++ ex10.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+hate_cat = "\rHate cat"
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(hate_cat)
