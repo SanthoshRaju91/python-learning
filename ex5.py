5e3d7286464e378ef00354168649a2a03ff954d4
Nagaraj Santhosh <santhosh.nagaraj@rakuten.com>
2020-01-26 19:46:56 +0530

    ex5: String formatting with f""

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Santhosh Raju'
+my_age = 28
+my_height = 74 # inches
+my_weight = 143.3 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} tall")
+print(f"He's {my_weight} pounds.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} depending on the coffee")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
